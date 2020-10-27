7f097703dadccef71b56da0a981564692b8dfdd0
fazalfarhan01 <fazal.farhan@gmail.com>
2020-10-27 11:57:28 +0530

    added plots to convs

--- EC57L/Experiment 5 - Properties of DFT 3/circularConvolutionUsingLinear.m
+++ EC57L/Experiment 5 - Properties of DFT 3/circularConvolutionUsingLinear.m
@@ -6,7 +6,7 @@ clc;
 xn = input("Enter the sequence 1: ");
 hn = input("Enter the sequence 2: ");
 
-LinearConv = linearConvolution(xn, hn);
+LinearConv = linearConvolution(xn, hn).';
 lengthOfCircConv = max(length(xn), length(hn));
 
 circularConv = zeros(1,lengthOfCircConv);
@@ -16,5 +16,35 @@ for index = 0:length(LinearConv)-1
     circularConv(index1) = circularConv(index1) + LinearConv(index + 1);
 end
 
+disp("Linear Convolution of x(n) with h(n) is: ");
+disp(LinearConv);
+
 disp("Circular Convolution using Linear Convolution is: ");
 disp(circularConv);
+
+
+%% PLOTS
+
+widthOfLine = 1;
+
+xAxis = 0:length(LinearConv)-1;
+subplot(1, 2, 1);
+localY = abs(LinearConv);
+stem(xAxis, localY, "lineWidth", widthOfLine);
+setAxisLimits(axis);
+setPlotAttributes("k \rightarrow", "Amplitude", "Linear Convolution of $$ x(n) $$ with $$ h(n) $$");
+for index = xAxis
+    localY1 = localY(index + 1);
+    text(index, localY1, strcat("\leftarrow", num2str(round(localY1, 1))));
+end
+
+xAxis = 0:length(circularConv)-1;
+subplot(1, 2, 2);
+localY = abs(circularConv);
+stem(xAxis, localY, "r", "lineWidth", widthOfLine);
+setAxisLimits(axis);
+setPlotAttributes("k \rightarrow", "Amplitude", "Circular Convolution of $$ x(n) $$ with $$ h(n) $$");
+for index = xAxis
+    localY1 = localY(index + 1);
+    text(index, localY1, strcat("\leftarrow", num2str(round(localY1, 1))));
+end
--- EC57L/Experiment 5 - Properties of DFT 3/linearConvolutionUsingCircular.m
+++ EC57L/Experiment 5 - Properties of DFT 3/linearConvolutionUsingCircular.m
@@ -1,3 +1,8 @@
+clear all;
+close all;
+clc;
+
+
 xn = input("Enter the first sequence: ");
 hn = input("Enter the second sequence: ");
 
@@ -9,7 +14,28 @@ linearConvLength = length_of_xn + length_of_hn - 1;
 xn = [xn zeros(1, linearConvLength - length_of_xn)];
 hn = [hn zeros(1, linearConvLength - length_of_hn)];
 
-linearConv = circularConvolution(xn, hn);
+linearConv = circularConvolution(xn, hn).';
+
+disp("x(n) and h(n) after making their length equal to length of Linear Conv. sequence");
+disp("x(n):");
+disp(xn);
+disp("h(n)");
+disp(hn);
 
 disp("Linear Convolution calculated using Circular Convolution is: ");
 disp(linearConv);
+
+%% PLOTS
+
+widthOfLine = 1;
+
+xAxis = 0:length(linearConv)-1;
+% subplot(1, 2, 1);
+localY = abs(linearConv);
+stem(xAxis, localY, "lineWidth", widthOfLine);
+setAxisLimits(axis);
+setPlotAttributes("k \rightarrow", "Amplitude", "Linear Convolution of $$ x(n) $$ with $$ h(n) $$ using Circular Convolution");
+for index = xAxis
+    localY1 = localY(index + 1);
+    text(index, localY1, strcat("\leftarrow", num2str(round(localY1, 1))));
+end
