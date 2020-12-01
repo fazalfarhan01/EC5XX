6153355ac5ef81b98d8373cb8208b9c4c5d78f3d
fazalfarhan01 <fazal.farhan@gmail.com>
2020-12-01 11:13:17 +0530

    adding simulink 58L

---
+++ EC57L/CCS/Debug/main.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+
+../main.c: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
---
+++ EC57L/CCS/main.c
@@ -0,0 +1,8 @@
+/*
+ * main.c
+ */
+#include <stdio.h>
+int main(void) {
+	printf("Hola Amigos..!!\n");
+	return 0;
+}
