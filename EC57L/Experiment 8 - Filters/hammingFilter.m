2ef3a7620b97c4e33f237c11d6c250b2727c666f
fazalfarhan01 <fazal.farhan@gmail.com>
2020-12-28 09:52:21 +0530

    corrected hanning and hamming

--- EC57L/Experiment 8 - Filters/hammingFilter.m
+++ EC57L/Experiment 8 - Filters/hammingFilter.m
@@ -3,40 +3,34 @@ close all;
 clc;
 lengthofsequence = input('Enter the length of Hamming Filter: ');
 filterType = input("Select An option for which Filter Type\n1. Low pass filter\n2. High pass \n3. Bandpass filter\n4. Stopband filter: ");
-frequencyType = input("Enter Frequency Type\nAnalog Domain => 1\nDigital Domain => 2 ");
+samplingFrequency = 2000;
+% disp("Selected Sampling Frequency is: ");
+% disp(samplingFrequency);
 
 switch filterType
     case 1
         Cuttofffreq = input('Enter the Cuttoff Frequency: ');
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "low", hamming(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq, "low", hamming(lengthofsequence + 1));
         freqz(hammingFilterOutput, 1, 1024);
     case 2
         Cuttofffreq = input('Enter the Cuttoff Frequency: ');
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "high", hamming(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq, "high", hamming(lengthofsequence + 1));
         freqz(hammingFilterOutput, 1, 1024);
     case 3
         FirstCutofffreq = input('Enter the Cutoff freq Omega(L): ');
         SecondCuttofffreq = input('Enter the cutoff freq Omega(U): ');
         Cuttofffreq = [FirstCutofffreq SecondCuttofffreq]
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "bandpass", hamming(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq, "bandpass", hamming(lengthofsequence + 1));
         freqz(hammingFilterOutput, 1, 1024);
     case 4
         typeOfFilter = "stop"
         FirstCutofffreq = input('Enter the Cutoff freq Omega(L): ');
         SecondCuttofffreq = input('Enter the cutoff freq Omega(U): ');
         Cuttofffreq = [FirstCutofffreq SecondCuttofffreq]
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "stop", hamming(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hammingFilterOutput = fir1(lengthofsequence, Cuttofffreq, "stop", hamming(lengthofsequence + 1));
         freqz(hammingFilterOutput, 1, 1024);
 end
--- EC57L/Experiment 8 - Filters/hanningFilter.m
+++ EC57L/Experiment 8 - Filters/hanningFilter.m
@@ -3,40 +3,34 @@ close all;
 clc;
 lengthofsequence = input('Enter the length of Hanning Filter: ');
 filterType = input("Select An option for which Filter Type\n1. Low pass filter\n2. High pass \n3. Bandpass filter\n4. Stopband filter: ");
-frequencyType = input("Enter Frequency Type\nAnalog Domain => 1\nDigital Domain => 2 ");
+samplingFrequency = 2000;
+% disp("Selected Sampling Frequency is: ");
+% disp(samplingFrequency);
 
 switch filterType
     case 1
         Cuttofffreq = input('Enter the Cuttoff Frequency: ');
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "low", hann(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq, "low", hann(lengthofsequence + 1));
         freqz(hanningFilterOutput, 1, 1024);
     case 2
         Cuttofffreq = input('Enter the Cuttoff Frequency: ');
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "high", hann(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq, "high", hann(lengthofsequence + 1));
         freqz(hanningFilterOutput, 1, 1024);
     case 3
         FirstCutofffreq = input('Enter the Cutoff freq Omega(L): ');
         SecondCuttofffreq = input('Enter the cutoff freq Omega(U): ');
         Cuttofffreq = [FirstCutofffreq SecondCuttofffreq]
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "bandpass", hann(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq, "bandpass", hann(lengthofsequence + 1));
         freqz(hanningFilterOutput, 1, 1024);
     case 4
         typeOfFilter = "stop"
         FirstCutofffreq = input('Enter the Cutoff freq Omega(L): ');
         SecondCuttofffreq = input('Enter the cutoff freq Omega(U): ');
         Cuttofffreq = [FirstCutofffreq SecondCuttofffreq]
-        if frequencyType == 1
-            Cuttofffreq = analogToDigital(Cuttofffreq);
-        end
-        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq / pi, "stop", hann(lengthofsequence + 1));
+        Cuttofffreq = Cuttofffreq / (samplingFrequency / 2);
+        hanningFilterOutput = fir1(lengthofsequence, Cuttofffreq, "stop", hann(lengthofsequence + 1));
         freqz(hanningFilterOutput, 1, 1024);
 end
